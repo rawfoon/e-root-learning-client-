22c799e72fc5c19278172d23174e2b01c166dec7
rawfoon <mgrawfoon@gmail.com>
2022-10-25 10:50:23 +0600

    auth context set up

---
+++ src/Context/AuthProvider/AuthProvider.js
@@ -0,0 +1,19 @@
+import React, { Children, createContext } from 'react';
+
+
+export const AuthContext = createContext()
+
+const AuthProvider = ({children}) => {
+
+
+    const authInfo = {}
+    return (
+        <div>
+            <AuthContext.Provider value={authInfo}>
+                {children}
+            </AuthContext.Provider>
+        </div>
+    );
+};
+
+export default AuthProvider;
