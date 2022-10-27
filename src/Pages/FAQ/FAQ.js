2acc43800ff3f530d6b00a3ab8447bb11265a4d0
rawfoon <mgrawfoon@gmail.com>
2022-10-27 23:54:46 +0600

    faq finished

--- src/Pages/FAQ/FAQ.js
+++ src/Pages/FAQ/FAQ.js
@@ -8,19 +8,19 @@ const FAQ = () => {
             <section className="dark:bg-gray-800 dark:text-gray-100 pb-[300px]">
 	<div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8" style={{maxWidth: '1440px'}}>
 		<h2 className="text-2xl font-semibold sm:text-4xl">Frequently Asked Questions</h2>
-		<p className="mt-4 mb-8 dark:text-gray-400">Sagittis tempor donec id vestibulum viverra. Neque condimentum primis orci at lacus amet bibendum.</p>
+		<p className="mt-4 mb-8 dark:text-gray-400">There are some frequently asked Questions answerd bellow</p>
 		<div className="space-y-4">
 			<details className="w-full border rounded-lg">
-				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">Ex orci laoreet egestas sapien magna egestas scelerisque?</summary>
-				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">Lectus iaculis orci metus vitae ligula dictum per. Nisl per nullam taciti at adipiscing est. </p>
+				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">Why we are best?</summary>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">We are best for providing the best services </p>
 			</details>
 			<details className="w-full border rounded-lg">
-				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">Lorem at arcu rutrum viverra metus sapien venenatis lobortis odio?</summary>
-				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">Tincidunt ut hac condimentum rhoncus phasellus nostra. Magna porttitor egestas tincidunt neque vehicula potenti. </p>
+				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">Why do I choose you?</summary>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">You'll get the best support from us </p>
 			</details>
 			<details className="w-full border rounded-lg">
-				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">Eleifend feugiat sollicitudin laoreet adipiscing bibendum suscipit erat?</summary>
-				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">Justo libero tellus integer tincidunt justo semper consequat venenatis aliquet imperdiet. Ultricies urna proin fusce nulla pretium sodales vel magna et massa euismod vulputate sed. </p>
+				<summary className="px-4 py-6 focus:outline-none focus-visible:ring-violet-400">Is it worthy?</summary>
+				<p className="px-4 py-6 pt-0 ml-4 -mt-4 dark:text-gray-400">Yes,  it is worthy for you... </p>
 			</details>
 		</div>
 	</div>
